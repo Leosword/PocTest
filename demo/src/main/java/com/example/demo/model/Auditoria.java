6e2d69287e2de6b2e0a63bcd2353315509c958e3
antonio145 <sjonath145@gmail.com>
2021-02-05 18:24:00 -0300

    Create Auditoria.java
    Classe AuditoriaModel criada

---
+++ demo/src/main/java/com/example/demo/model/Auditoria.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+
+public class Auditoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idAuditoria;
+
+	@Column(name = "acao", nullable = false)
+	private String acao;
+
+	@Column(name = "data", nullable = false)
+	private Date data;
+
+	@Column(name = "hora", nullable = false)
+	private Date hora;
+
+	@Column(name = "valor", nullable = false)
+	private BigDecimal valor;
+
+	@Column(name = "valorAnterior", nullable = false)
+	private BigDecimal valorAnterior;
+
+	@Column(name = "novovalor", nullable = false)
+	private BigDecimal novoValor;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "Usuario_idUsuario")
+	private UsuarioModel usuarioModel;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "Usuario_Conta_idConta")
+	private ContaModel contaModel;
+	
+}
