b2f856ce59d26e7c99b35b0e9716c0ee7982abb7
Gabrieljipa7 <78506332+Gabrieljipa7@users.noreply.github.com>
2021-02-08 18:31:38 -0300

    configuracao sql
    tentativa de configurar o SQL "Falha"

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,6 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @SpringBootApplication
 public class DemoApplication {
+	 @PostConstruct
+     void started() {
+       TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
+	 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
