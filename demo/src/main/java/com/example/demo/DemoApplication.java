b0e60889206b16eb63ad00256ad0e53a1fa5f87d
viniciusSalvador <78506414+viniciusSalvador@users.noreply.github.com>
2021-02-09 15:37:38 -0300

    gabriel vadia
    tonho CALA A BOCA

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
-import java.util.TimeZone;
 
-import javax.annotation.PostConstruct;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @SpringBootApplication
 public class DemoApplication {
-	 @PostConstruct
-     void started() {
-       TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
-	 }
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
