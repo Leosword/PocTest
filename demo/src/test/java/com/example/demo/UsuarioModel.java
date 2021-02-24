783e69454ed38985c2ee7ab1bddb0f840e81deb9
viniciusSalvador <78506414+viniciusSalvador@users.noreply.github.com>
2021-02-24 13:55:16 -0300

    pagamentoModel e repositoryModel
    criadas

---
+++ demo/src/test/java/com/example/demo/UsuarioModel.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class UsuarioModel {
+
+	
+	
+}
