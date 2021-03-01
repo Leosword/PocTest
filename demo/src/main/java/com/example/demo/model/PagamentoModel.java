3e75b25a4012a737348320d662bc5b0477f03929
viniciusSalvador <78506414+viniciusSalvador@users.noreply.github.com>
2021-03-01 13:42:43 -0300

    data.sql att

--- demo/src/main/java/com/example/demo/model/PagamentoModel.java
+++ demo/src/main/java/com/example/demo/model/PagamentoModel.java
@@ -5,9 +5,13 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -42,4 +46,5 @@ public class PagamentoModel {
 		this.qtdePagamento = qtdePagamento;
 		return this;
 	}
+	
 }
