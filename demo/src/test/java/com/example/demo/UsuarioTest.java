2467d14418a803a6778c9212c1f10920a7fa2fc2
viniciusSalvador <78506414+viniciusSalvador@users.noreply.github.com>
2021-03-01 17:30:21 -0300

    USUARIO TEST ATT.

--- demo/src/test/java/com/example/demo/UsuarioTest.java
+++ demo/src/test/java/com/example/demo/UsuarioTest.java
@@ -101,8 +101,7 @@ class UsuarioTest {
 
 
 	@Test
-	public void obterTodosUsuarios() {
-	
+	public void obterTodosUsuariosOK() {
 		List<UsuarioModel> usuario1 = new ArrayList<UsuarioModel>();
 		usuario1.add(usuarioModel1);
 		Mockito.when(usuarioRepository.findAll()).thenReturn(usuario1);
@@ -112,7 +111,17 @@ class UsuarioTest {
 	}
 	
 	@Test
-	public void ObterPorIdOK() {
+	public void obterTodosUsuariosNotOK() {
+		List<UsuarioModel> usuario1 = new ArrayList<UsuarioModel>();
+		usuario1.add(null);
+		Mockito.when(usuarioRepository.findAll()).thenReturn(usuario1);
+		List<UsuarioModel> obterTodos = new ArrayList<>();
+		obterTodos = usuarioServiceImpl.obterTodos();
+		assertEquals(obterTodos, usuario1);
+	}
+	
+	@Test
+	public void ObterUsuarioPorIdOK() {
 		UsuarioModel usuarioModel1 = new UsuarioModel();
 		usuarioModel1.setIdUsuario(1L);
 		Mockito.when(usuarioRepository.obterPorID(usuarioModel1.getIdUsuario())).thenReturn(usuarioModel1);
@@ -120,10 +129,119 @@ class UsuarioTest {
 		assertEquals(obterPorID, usuarioModel1);
 	}
 
-
 	@Test
-	void contextLoads() {		
+	public void ObterUsuarioPorIdNotOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setIdUsuario(null);
+		Mockito.when(usuarioRepository.obterPorID(usuarioModel1.getIdUsuario())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorID = usuarioServiceImpl.obterPorID(usuarioModel1.getIdUsuario());
+		assertEquals(obterPorID, usuarioModel1);
+	}
+	
+	@Test
+	public void obterUsuarioPorNomeOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setNome("Tonho");
+		Mockito.when(usuarioRepository.obterPorNome(usuarioModel1.getNome())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorNome = usuarioServiceImpl.obterPorNome(usuarioModel1.getNome());
+		assertEquals(obterPorNome, usuarioModel1);
+	}
+	
+	
+	@Test
+	public void obterUsuarioPorNomeNotOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setNome(null);
+		Mockito.when(usuarioRepository.obterPorNome(usuarioModel1.getNome())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorNome = usuarioServiceImpl.obterPorNome(usuarioModel1.getNome());
+		assertEquals(obterPorNome, usuarioModel1);
+	}
+	
+	
+	@Test
+	public void obterUsuarioPorCPFOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setNome("111.111.111-11");
+		Mockito.when(usuarioRepository.obterUsuarioPorCPF(usuarioModel1.getCpf())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorCPF = usuarioServiceImpl.obterUsuarioPorCPF(usuarioModel1.getCpf());
+		assertEquals(obterPorCPF, usuarioModel1);
+	}
+	
+	@Test
+	public void obterUsuarioPorCPFNotOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setCpf(null);
+		Mockito.when(usuarioRepository.obterUsuarioPorCPF(usuarioModel1.getCpf())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorCPF = usuarioServiceImpl.obterUsuarioPorCPF(usuarioModel1.getCpf());
+		assertEquals(obterPorCPF, usuarioModel1);
+	}
+	
+	
+	@Test
+	public void obterUsuarioPorRGOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setRg("11.111.111-1");
+		Mockito.when(usuarioRepository.obterUsuarioPorRG(usuarioModel1.getRg())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorRG = usuarioServiceImpl.obterUsuarioPorRG(usuarioModel1.getRg());
+		assertEquals(obterPorRG, usuarioModel1);
+	}
+	
+	@Test
+	public void obterUsuarioPorRGNotOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setRg(null);
+		Mockito.when(usuarioRepository.obterUsuarioPorRG(usuarioModel1.getRg())).thenReturn(usuarioModel1);
+		UsuarioModel obterPorRG = usuarioServiceImpl.obterUsuarioPorRG(usuarioModel1.getRg());
+		assertEquals(obterPorRG, usuarioModel1);
+	}
+	
+	@Test
+	public void obterUsuarioPorEstadosOK() {
+		List<UsuarioModel> usuario1 = new ArrayList<UsuarioModel>();
+		usuario1.add(usuarioModel1);
+		Mockito.when(usuarioRepository.obterPorEstado(usuarioModel1.getEstado())).thenReturn(usuario1);
+		List<UsuarioModel> obterPorEstado = new ArrayList<>();
+		obterPorEstado = usuarioServiceImpl.obterPorEstado(usuarioModel1.getEstado());
+		assertEquals(obterPorEstado, usuario1);
+	}
+	
+	@Test
+	public void obterUsuarioPorEstadosNotOK() {
+		List<UsuarioModel> usuario1 = new ArrayList<UsuarioModel>();
+		usuario1.add(null);
+		Mockito.when(usuarioRepository.obterPorEstado(usuarioModel1.getEstado())).thenReturn(usuario1);
+		List<UsuarioModel> obterPorEstado = new ArrayList<>();
+		obterPorEstado = usuarioServiceImpl.obterPorEstado(usuarioModel1.getEstado());
+		assertEquals(obterPorEstado, usuario1);
 	}
 
+	@Test
+	public void salvarUsuarioOK() {
+		UsuarioModel usuarioModel1 = new UsuarioModel();
+		usuarioModel1.setIdUsuario(1l);
+		Mockito.when(usuarioRepository.save(usuarioModel1)).thenReturn(usuarioModel1);
+		UsuarioModel salvarUser = usuarioServiceImpl.salvarUsuario(usuarioModel1);
+		assertEquals(salvarUser, usuarioModel1);
+	}
+	
+//	@Test
+//	public void deletarUsuarioOK() {
+//		UsuarioModel usuario1 = new UsuarioModel();
+// 		usuario1 = 	usuarioServiceImpl.deletarUsuario(usuario1.getIdUsuario());
+//	}
+	
+//	@Test
+//	public void atualizarUsuarioOK() {
+//		UsuarioModel usuario1 = new UsuarioModel();
+//		usuario1 = usuarioRepository.obterPorID(idUsuario);
+//		Mockito.when(usuarioRepository.save(usuario1)).thenReturn(usuario1);
+//		UsuarioModel usuarioTeste = usuarioServiceImpl.atualizarUsuario(usuario1);
+//		assertEquals(usuarioTeste, usuario1);
+//	}
+//	
+	@Test
+	void contextLoads() {	
+		
+	}
 }
 
