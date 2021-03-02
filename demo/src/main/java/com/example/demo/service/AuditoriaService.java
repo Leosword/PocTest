cd65d28a8d1a63e230148da6573aad25eea73a46
tonhao <sjonath145@gmail.com>
2021-03-02 15:54:11 -0300

    att auditoria

--- demo/src/main/java/com/example/demo/service/AuditoriaService.java
+++ demo/src/main/java/com/example/demo/service/AuditoriaService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.example.demo.model.PagamentoModel;
 import com.example.demo.model.TransacoesModel;
 
 public interface AuditoriaService {
@@ -8,4 +12,8 @@ public interface AuditoriaService {
 
 	TransacoesModel obterTrasacoesUsuario(String nome);
 
+	 List<TransacoesModel> TipoDeTransacao(String nome, Date dataDaTransacao);
+	 
+	 PagamentoModel obterPagamentoId(Long idPag);
+	 
 }
